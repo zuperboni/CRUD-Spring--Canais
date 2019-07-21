e0cf97625cc95a6785df6ca2396cf431796ea4cd
monicaroline <moni_caroline@yahoo.com.br>
2019-07-21 13:48:39 -0300

    Removendo pasta da estrutura anterior

--- Canais/src/main/java/com/canais/online/Canais/usecases/AlterarEndereco.java
+++
@@ -1,9 +0,0 @@
-package com.canais.online.Canais.usecases;
-
-public class AlterarEndereco {
-
-	public AlterarEndereco() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- Canais/src/main/java/com/canais/online/Canais/usecases/ConsultarMunicipios.java
+++
@@ -1,9 +0,0 @@
-package com.canais.online.Canais.usecases;
-
-public class ConsultarMunicipios {
-
-	public ConsultarMunicipios() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
